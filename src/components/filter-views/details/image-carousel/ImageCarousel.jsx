3b100b9a5ae94e2bae8b4655b251606ac74b24ef
jeffersonwilliam <wakesseh@gmail.com>
2023-05-16 12:28:47 -0400

    added images on carousel

--- src/components/filter-views/details/image-carousel/ImageCarousel.jsx
+++ src/components/filter-views/details/image-carousel/ImageCarousel.jsx
@@ -6,7 +6,15 @@ import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-// import { IoChevronBackCircleOutline } from 'react-icons/io';
+import r1 from '../../../../assets/r1.jpg';
+import r2 from '../../../../assets/r2.jpg';
+import r3 from '../../../../assets/r3.jpg';
+import r4 from '../../../../assets/r4.jpg';
+import r5 from '../../../../assets/r5.jpg';
+import r6 from '../../../../assets/r6.jpg';
+import r7 from '../../../../assets/r7.jpg';
+import r8 from '../../../../assets/r8.jpg';
+import r9 from '../../../../assets/r9.jpg';
 import {
   MdOutlineArrowBackIosNew,
   MdOutlineArrowForwardIos,
@@ -20,39 +28,47 @@ const ImageCarousel = () => {
         <MdOutlineArrowBackIosNew />
       </div>
       <div className='menu-img-carousel'>
-        {/* <div className='swiper-button image-swiper-button-next'>
-        <MdOutlineArrowForwardIos />
-      </div>
-
-      <div className='swiper-button image-swiper-button-prev'>
-        <MdOutlineArrowBackIosNew />
-      </div> */}
-
         <Swiper
           slidesPerView={6}
           spaceBetween={30}
-          // freeMode={true}
-          // navigation={true}
           navigation={{
             nextEl: '.image-swiper-button-next',
             prevEl: '.image-swiper-button-prev',
             disabledClass: 'swiper-button-disabled',
           }}
-          pagination={{
-            clickable: true,
-          }}
+          // pagination={{
+          //   clickable: true,
+          // }}
           modules={[Pagination, Navigation]}
           className='mySwiper'
         >
-          <SwiperSlide>Slide 1</SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
-          <SwiperSlide>Slide 5</SwiperSlide>
-          <SwiperSlide>Slide 6</SwiperSlide>
-          <SwiperSlide>Slide 7</SwiperSlide>
-          <SwiperSlide>Slide 8</SwiperSlide>
-          <SwiperSlide>Slide 9</SwiperSlide>
+          <SwiperSlide>
+            <img src={r1} alt='' />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={r2} alt='' />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={r4} alt='' />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={r3} alt='' />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={r5} alt='' />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={r6} alt='' />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={r7} alt='' />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={r8} alt='' />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={r9} alt='' />
+          </SwiperSlide>
         </Swiper>
       </div>
 
